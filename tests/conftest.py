117ce311f0042e6a2b953af26efdf6173ad466af
Godzdor8 <godzdor228@gmail.com>
2025-04-14 17:04:59 +0300

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
+import sys
+import os
+
+# Добавляем папку 'app' в путь поиска модулей
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+
 import pytest
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import app  # Теперь модуль 'app' должен быть найден
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def client():
+    # Создаем клиент для тестирования
     return TestClient(app)
